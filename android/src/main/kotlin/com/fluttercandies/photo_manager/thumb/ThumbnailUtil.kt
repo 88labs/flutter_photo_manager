13b89077b3c744f6110d230611cd2ef7c9826841
hiroki-nakamori <hiroki.nakamori@andpad.co.jp>
2023-04-20 14:24:26 +0900

    add import.
    (cherry picked from commit 2315bde658be99c9e12a0bc526aa34bc3bb466c8)

--- android/src/main/kotlin/com/fluttercandies/photo_manager/thumb/ThumbnailUtil.kt
+++ android/src/main/kotlin/com/fluttercandies/photo_manager/thumb/ThumbnailUtil.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.net.Uri
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.FutureTarget
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.signature.ObjectKey
